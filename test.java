98690e58c498db5a0c9ea63ca29bd70eda9335ea
movila <movila.mix@gmail.com>
2013-11-18 15:36:28 -0500

    update java file from local machine

--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class test {
 	public void main(String[] args, int argnum) {
 		// This is a HelloWorld program
 		System.out.println("Hello World");
+		/* Make some local change on Vbox machine */
 	}
 }
